0512a610a340a9f3673bb2409300bb4a62b8d008
CristianoOk <105819236+CristianoOk@users.noreply.github.com>
2025-05-16 00:27:06 -0300

    Primer commit - calculadora React

---
+++ src/components/calculatorScreen.jsx
@@ -0,0 +1,15 @@
+import { useAppContext } from "./calculatorState";
+
+export default function CalculatorScreen() {
+  const calculator = useAppContext();
+
+  return (
+    <div className="calculatorScreen">
+      <div>
+        Memory: {calculator.memory}
+        Operation: {calculator.operation}
+      </div>
+      <div className="calculatorCurrentValue">{calculator.currentValue}{calculator.isDecimal ? "." : ""}</div>
+    </div>
+  )
+}
